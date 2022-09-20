c90e16fe27c822fb7ff09bab1d3ecdc92a3cdb17
ait27ru <ait27ru@gmail.com>
2022-09-20 23:19:05 +1200

    Add details on creating Azure service principal

--- working/scripts/variables.tf
+++ working/scripts/variables.tf
@@ -11,6 +11,11 @@ variable admin_username {
   default = "linux_admin"
 }
 
+# To create a service principal:
+# 1. az account show
+# 2. az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/<id field from previous command output>"
+# use appId for client_id and password for client_secret
+
 variable client_id {
 }
 
